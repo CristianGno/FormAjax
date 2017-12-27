c2ccec1addc0df21601efec9c10d33435e736a66
Cristian Galeano <cristian.galeano1913@gmail.com>
2017-12-26 21:02:45 -0500

    Update formAjax.js

--- formAjax.js
+++ formAjax.js
@@ -1,4 +1,33 @@
-/*/
-function FormAjax(){
-  
+$(document).ready(function() {
+    $('#btnSubmit').click(function(e) {
+        e.preventDefault();
+        var idForm = $(this).attr('aria-form');
+        var form = new FormData(document.getElementById(idForm));
+        var action = $('#' + idForm).attr('action');
+        //console.log(action);
+        //
+        formAjax(action, form);
+    });
+});
+
+function formAjax(action, form) {
+    var posicion = $('#message').offset().top;
+    $.ajax({
+        url: action,
+        type: 'POST',
+        data: form,
+        contentType: false,
+        processData: false,
+        cache: false,
+        beforeSend: function(){
+            $('#btnSubmit').after('<p><img class=id="ajax-loader" src="./images/ajax-loader.gif"></p>');
+        }
+    }).done(function(r) {
+        $('img#ajax-loader').slideUp('slow');
+        $('html, body').animate({
+            scrollTop: posicion - 20
+        }, 2000);
+        $('#message').slideDown('slow');
+        $('#message').html(r);
+    }); /*END ajax*/
 }
